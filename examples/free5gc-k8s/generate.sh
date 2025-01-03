97b0702f5e6562bfac57a8af40a7fdc533c6c439
Philippe Merle <philippe.merle@inria.fr>
2025-01-03 10:33:20 +0100

    Use kustomize for generating the diagram

--- examples/free5gc-k8s/generate.sh
+++ examples/free5gc-k8s/generate.sh
@@ -1,7 +1,22 @@
 #! /bin/sh
 
+alias kubectl="minikube kubectl"
+
 # Clone free5gc-k8s
 git clone https://github.com/niloysh/free5gc-k8s.git
 
+# Kustomize free5gc-k8s
+generated_filenames=""
+for kf in `ls free5gc-k8s/*/kustomization.yaml`
+do
+  part=$(basename $(dirname $kf))
+  generated_filename="free5gc-k8s-$part.yaml"
+  kubectl kustomize free5gc-k8s/$part > ${generated_filename}
+  generated_filenames="${generated_filenames} ${generated_filename}"
+done
+
 # Generate the free5gc-k8s architecture diagram.
-kube-diagrams -c KubeDiagrams.yml -o free5gc-k8s-diagram $(grep "apiVersion: " -rl --exclude="kustomization.yaml" free5gc-k8s) free5gc-k8s-config-maps.yaml
+kube-diagrams -c KubeDiagrams.yml -o free5gc-k8s-diagram ${generated_filenames}
+
+# Remove generated files
+rm ${generated_filenames}
